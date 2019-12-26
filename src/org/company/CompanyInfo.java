6599f612829d1db61c0d8c61aa42794a2166d64a
sathishprem <nsathishkumar2016>
2019-12-26 11:40:36 +0530

    coding done

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class CompanyInfo {
+	public static void main(String[] args) {
+		for(int i=1;i<=3;i++) {
+
+			for(int j=i+1;j<=3;j++)
+			
+			
+				
+				System.out.println(j);
+			System.out.println(i);
+		
+	}
+	}
+	}
+	
+	
+	
+
+
+
+
